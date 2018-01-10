e686e7cf0695ac8d53e6b12462af06a2d5f96b26
Clayton Ezzell <Cezzell@MacBook-Pro-4.local>
2018-01-10 16:37:51 -0700

    More Problem54

--- Problems51-60/ProjectEulerQ54.py
+++ Problems51-60/ProjectEulerQ54.py
@@ -70,7 +70,84 @@ def FourOfAKind(Value, Suit):
 	# No four of a kind
 	return (False, -1)
 
-def
+def FullHouse(Value):
+	"""
+	Checking for a full house. Find and hold onto pair and triple.
+	"""
+	#Three helper variables to hold onto important information
+	Triple = []
+	Pair = []
+	count = 0
+
+	# Iterate through values
+	for val in Value:
+		#Check if the next value is already accounted for in a Pair or Triple
+		if val in Triple or val in Pair:
+			continue
+		else:
+			for y in Value:
+				if y == val:
+					#Count all occurences of this value
+					count++
+		# Check if we found a triple and pair yet
+		if len(Pair) != 0 and len(Triple) != 0:
+			#Return a triple for tie break purposes
+			return (True, Triple[0], Pair[0])
+		if count == 2:
+			Pair.append(val)
+		else if count == 3:
+			Triple.append(val)
+		else:
+			return False
+
+def Flush(Suit):
+	"""
+	Checking for a Flush. Simply make sure all the suits match.
+	"""
+	CheckSuit = Suit[0]
+	for suit in Suit:
+		if CheckSuit != suit:
+			return False
+	return True
+
+def ResolveFlushTie(Value1, Value2):
+	"""
+	Since a Flush can be any values, we have to check every value...
+	"""
+	CheckValues = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' ]
+	for check in CheckValues:
+		if check in Value1 and check not in Value2:
+			return 1
+		if check not in Value1 and check in Value2:
+			return 2
+	return -1
+
+def Straight(Value):
+	"""
+	Checking for a straight. Starting from the high end to more easily find high card.
+	"""
+
+	CheckValues =['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' ]
+
+	for x in range(8):
+		if CheckValues[x] in Value:
+			for y in range(1,5):
+				if CheckValues[x+y] != Value:
+					return (False, -1)
+			return(True, CheckValues[x+4])
+
+def ThreeKind(Value):
+	"""
+	Checking for Three of a kind. Flush Checker can be used for 
+	"""
+	count = 0
+	for val in Value[:3]:
+		for x in Value:
+			if count == 3:
+				return (True, val)
+			else if val == x:
+				count++
+	return (False, -1)
 
 
 
